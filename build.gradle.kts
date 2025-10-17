ed85d115c7a1e2d38d4a6f76e34477456cbb9b75
mingdodev <mingdo.dev@gmail.com>
2025-10-18 02:40:20 +0900

    Validation 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.exposed:exposed-core:1.0.0-rc-2")
